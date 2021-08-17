761bcbe49cf18627142418893f3e9ccbbccca4d1
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2021-08-17 14:57:08 -0500

    4/4 of exploraory exercise. Added telco_data

--- get_db.py
+++ get_db.py
@@ -14,3 +14,12 @@ iris_sql="SELECT measurements.measurement_id, \
 
 def get_iris_data():
     return pd.read_sql(iris_sql,get_connection('iris_db'))
+
+telco_sql = "select *\
+    FROM customers\
+        RIGHT JOIN contract_types USING(contract_type_id)\
+            RIGHT JOIN internet_service_types USING(internet_service_type_id)\
+                RIGHT JOIN payment_types USING(payment_type_id);"
+
+def get_telco_data():
+    return pd.read_sql(telco_sql,get_connection('telco_churn'))
